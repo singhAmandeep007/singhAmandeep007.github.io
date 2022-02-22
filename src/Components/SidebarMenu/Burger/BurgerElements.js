f8f9115a08bff196e8cbceac8e9aee504c8cd27e
Grimm0803 <55043082+amandeepmicro@users.noreply.github.com>
2022-02-22 08:21:49 +0530

    fixed burger styling , rem -> px

--- src/Components/SidebarMenu/Burger/BurgerElements.js
+++ src/Components/SidebarMenu/Burger/BurgerElements.js
@@ -2,13 +2,12 @@ import styled from 'styled-components';
 
 export const BurgerContainer = styled.button`
   position: fixed;
-  top: 2rem;
-  left: 2rem;
+  top: 20px;
+  left: 20px;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  width: 2rem;
-  height: 2rem;
+  height: 20px;
   background: transparent;
   border: none;
   cursor: pointer;
@@ -19,9 +18,9 @@ export const BurgerContainer = styled.button`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    width: 4rem;
-    height: 4rem;
-    border: 5px solid var(--color-primary);
+    width: 50px;
+    height: 50px;
+    border: 4px solid var(--color-primary);
     border-radius: 50%;
     position: absolute;
   }
@@ -32,20 +31,20 @@ export const BurgerContainer = styled.button`
   span {
     position: relative;
 
-    width: 2rem;
-    height: 0.25rem;
+    width: 25px;
+    height: 2.5px;
     background: var(--color-primary);
-    border-radius: 10px;
+    border-radius: 5px;
     transition: all 0.3s linear;
 
-    transform-origin: 1px;
+    transform-origin: 3px;
     :first-child {
       transform: ${({ $open }) => ($open ? 'rotate(45deg)' : 'rotate(0)')};
     }
     :nth-child(2) {
       opacity: ${({ $open }) => ($open ? '0' : '1')};
       transform: ${({ $open }) =>
-        $open ? 'translateX(20px)' : 'translateX(0)'};
+        $open ? 'translateX(25px)' : 'translateX(0)'};
     }
     :nth-child(3) {
       transform: ${({ $open }) => ($open ? 'rotate(-45deg)' : 'rotate(0)')};
