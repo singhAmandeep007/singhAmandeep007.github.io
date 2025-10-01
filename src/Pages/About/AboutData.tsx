1d654e986a77519cfcd89b35764b9475575484ad
Amandeep Singh <55043082+singhAmandeep007@users.noreply.github.com>
2025-10-02 01:12:40 +0530

    fix: update job title in AboutData to reflect current position

--- src/Pages/About/AboutData.tsx
+++ src/Pages/About/AboutData.tsx
@@ -4,7 +4,7 @@ import profileImg from "/profilePic.webp";
 export const data = {
   profileImg: profileImg,
   content: `Hello, I’m Amandeep Singh. I’m a **full-stack** engineer with a strong **front-end** focus specializing in developing and designing exceptional and beautiful web applications. 
-	\n I’m currently working as a Frontend Engineer at **[Egnyte](https://egnyte.com/)**.
+	\n I’m currently working as an Engineer at **[Egnyte](https://egnyte.com/)**.
 	\n My experience as a self taught proficient web developer allows me to come up with smart solutions to technical challenges.
 	\n I’m a coding enthusiast and passionate about learning and implementing web technologies.
 	\n I enjoy travelling and playing sports, I also write articles about web on **[Coders League](https://codersleague.onrender.com/)**.`,
